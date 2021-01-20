79525c6f70fe98c26eee689b1eac1f874d9fd692
Cristopher <cr.rojasmunoz@gmail.com>
2021-01-19 22:13:54 -0300

    trabajando con nombre de variables

---
+++ cristopher_rojas/js-variables/dandoElNombreCorrecto.js
@@ -0,0 +1,6 @@
+//La variable es llamada así por que describe de forma efectiva el nombre de nuestro planeta
+const ourPlanet = 'Planet Earth';
+//La variable es llamada así porque referencia de forma acertada el usuario actual.
+let currentUser = 'cristopher.rojas';
+
+
