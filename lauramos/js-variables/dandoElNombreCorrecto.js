840f7a7c8d7daaa93bf3c02d2823f2b7af960327
Laura Ramos <lauraramos.globant@latam.com>
2021-01-19 20:17:23 -0500

    create planet_earth and username variables

---
+++ lauramos/js-variables/dandoElNombreCorrecto.js
@@ -0,0 +1,8 @@
+const PLANET_EARTH = "earth"
+let userName = "Laura"
+
+// las variables las llamé así debido a que la constante debe ser en mayúscula y estamos hablando especificamente del planeta tierra y userName es muy descriptivo y su valor va a cambiar, por esto es let, igualmentes ambas en inglés como buena práctica//
+
+
+console.log(PLANET_EARTH)
+console.log(userName)
