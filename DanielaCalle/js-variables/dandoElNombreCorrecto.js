3c194e5ee2caf565b5ab3d0e15953fb3276eadce
Daniela <dany0913@hotmail.com>
2021-01-19 20:31:18 -0500

    Correction of the second example accorging to the class - in the alert and console

--- DanielaCalle/js-variables/dandoElNombreCorrecto.js
+++ DanielaCalle/js-variables/dandoElNombreCorrecto.js
@@ -3,9 +3,9 @@
 
 // Both variables are in english for JS conventions
 
-const outPlanetName="Tierra"; // The first variable is named planet because the idea is to be mnemonic
-let currentUser="Daniela Calle"; // The second variable is named currentUser for the exact samen reason of the one before
+const ourPlanetName="Tierra"; // The first variable is named planet because the idea is to be mnemonic
+const currentUserName="Daniela Calle"; // The second variable is named currentUser for the exact samen reason of the one before
 
 //Print the variables in console and alert
-console.log("Usuario actual: "+currentUser+"\nPlaneta en el que habita: "+ planet);
-alert("Usuario actual: "+currentUser+". \nPlaneta en el que habita: "+ planet);
+console.log("Usuario actual: "+currentUserName+"\nPlaneta en el que habita: "+ ourPlanetName);
+alert("Usuario actual: "+currentUserName+". \nPlaneta en el que habita: "+ ourPlanetName);
