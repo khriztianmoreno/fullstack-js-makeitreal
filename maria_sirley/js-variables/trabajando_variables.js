17ad0dae3d04ee22291d4022f257a3cb4ccf8f0a
Maria_Tangarife <mariasirleytangarifegil@gmailcom>
2021-01-19 20:34:50 -0500

    maria_commits_en_js

--- maria_sirley/js-variables/trabajando_variables.js
+++ maria_sirley/js-variables/trabajando_variables.js
@@ -5,3 +5,9 @@ const mariaName = 'John';
 admin = mariaName;
 
 console.log(admin);
+
+/* Nombre la variable namePlanet por que es una
+ manera mas espesifica de referenciar que es el dato 
+ que contiene mi variable */
+
+const namePlanet ='Tierra';
