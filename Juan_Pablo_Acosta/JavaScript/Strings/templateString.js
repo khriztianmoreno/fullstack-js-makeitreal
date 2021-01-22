b23d237dcc8c6c8bfdd2f2efa647b2716bb92327
Juan Pablo Acosta <pabloacosta.b17@gmail.com>
2021-01-21 19:04:10 -0500

    TemplateString.js excercise created

---
+++ Juan_Pablo_Acosta/JavaScript/Strings/templateString.js
@@ -0,0 +1,6 @@
+const name="Juan Pablo Acosta";
+let age=23;
+const profession="Ingeniero mecatrónico";
+
+console.log(`Mi nombre es ${name}, tengo ${age} años y soy ${profession} de la Universidad EIA en Antioquia.\nActualmente trabajo en la empresa ERCO ENERGIA 
+lider de almacenamiento de energía.`)
