feae5d594bb9105f8ce6d8d8b9eaeea231343755
DMRV <dimarojas31@gmail.com>
2021-01-21 19:05:03 -0500

    Exercise template string Diana

---
+++ diana_rojas/templateString/templatestring.js
@@ -0,0 +1,4 @@
+let myName='Diana Rojas';
+let myAge='25';
+let myProfession='Ingeniera Mecatrónica'
+console.log(`Mi nombre es ${myName} tengo ${myAge} años, soy ${myProfession} actualmente trabajo en una empresa de consultoria de BigData`)
