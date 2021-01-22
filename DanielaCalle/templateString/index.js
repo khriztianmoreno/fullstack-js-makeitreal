716389e08ecf67a26cafaa746d6f9c85446ff70d
Daniela <dany0913@hotmail.com>
2021-01-21 19:02:50 -0500

    Template String Example - Daniela Calle

---
+++ DanielaCalle/templateString/index.js
@@ -0,0 +1,6 @@
+let myName = "Daniela Calle";
+let myAge = "26";
+let myCareer="Ingeniera de Sistemas";
+let myCurrentJob="Senior Technology Risk";
+
+console.log(`¡Hola! Mi nombre es ${myName}, tengo ${myAge} años y estudié ${myCareer}, actualmente me desempeño como ${myCurrentJob}`);
