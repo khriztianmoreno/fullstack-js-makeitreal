ada0c71df68b6653c3c22f2673e51d5d7357c505
Laura Monsalve <lmonsalve2219@gmail.com>
2021-01-24 12:01:02 -0500

    mistake solution

---
+++ laura_monsalve/jsVariables/templateString/professionalProfile.js
@@ -0,0 +1,5 @@
+const userName = "Laura Monsalve ";
+let userAge = "25 años ";
+let userProfession = "Project Maganer ";
+
+console.log(`Hola, mi nombre es ${userName}, y tengo ${userAge} de edad. Actualmente trabajo en un compañía llamada Ecosystems Solutions donde me desempeño como ${userProfession}`)
