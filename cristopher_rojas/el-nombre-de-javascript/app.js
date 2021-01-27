303b49c66e57477afbcf4195a5ab95219aeec09d
Cristopher <cr.rojasmunoz@gmail.com>
2021-01-26 23:59:48 -0300

    Ejercicios Condicionales

---
+++ cristopher_rojas/el-nombre-de-javascript/app.js
@@ -0,0 +1,7 @@
+const consulta = prompt('¿Cuál es el nombre "Oficial" de javascript?');
+
+if (consulta === 'ECMAScript') {
+    alert('Correcto!')
+}else {
+    alert('Falso! el nombre correcto es ECMAScript');
+}
