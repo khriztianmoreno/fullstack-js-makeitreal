88a3126b0a7b5610fc9fd4df7fe7fc0daa2853dc
Isabella Serna <isabella.serna.ramirez@gmail.com>
2021-02-02 17:13:38 -0500

    operations subject

---
+++ isabella_serna/operations/operation.js
@@ -0,0 +1,128 @@
+////number conversion
+//
+//let a = Number(prompt("first number",1));
+//let b = Number(prompt("second number",2));
+//console.log(a + b);
+//
+//// If practices
+//
+//let message = prompt("What is the official name of JS?","ECMAScript");
+//const JAVASCRIPT_NAME = "ECMAScript";
+//
+//let answer = message === JAVASCRIPT_NAME ? "OK" : `Don't you know? is ${JAVASCRIPT_NAME}!`;
+//console.log(answer);
+//
+////Ternary if examples: 
+//
+//let inputNumber = parseInt(prompt("type a number",0));
+//
+//let answerNumber = 
+//    inputNumber > 0 ? 1: 
+//    inputNumber < 0 ? -1 : 
+//    inputNumber === 0 ? 0 :
+//    "error";
+//
+//console.log(answerNumber);
+//
+//let result;
+//result = a + b < 4 ? "down" : "up";
+//console.log(result);
+//
+
+
+
+//let browserName = prompt("type your browser name", "edge").toLowerCase();
+//let finalMessage;
+//
+//const OPERA = "opera";
+//const FIREFOX = "firefox";
+//const CHROME = "chrome";
+//const EDGE = "edge";
+//const SAFARI = "safari";
+//
+//
+//if (browserName === EDGE) {
+//    finalMessage = "You have Edge";
+//} else if (browserName === CHROME || browserName === FIREFOX || browserName === SAFARI
+//    || browserName === OPERA) {
+//    finalMessage = `It's okay. We support your browser (${browserName})!`;
+//} else {
+//    finalMessage = "We hope it looks okay.";
+//}
+//
+//console.log(finalMessage);
+
+let switchNumber = Number(prompt("insert number", 0));
+switch (switchNumber) {
+    case 0:
+        console.log(switchNumber);
+        break;
+    case 1:
+        console.log(switchNumber);
+        break;
+    case 2:
+    case 3:
+        console.log("2,3");
+        break
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
